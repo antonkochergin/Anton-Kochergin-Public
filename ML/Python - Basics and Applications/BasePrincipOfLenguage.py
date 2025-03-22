becbdde064584e10abd60b40a603aecd105f0020
Anton <antonkochergin4@gmail.com>
2025-03-22 14:02:28 +0400

    add new file Jupyter notebook for writing my conspects

--- ML/Python - Basics and Applications/BasePrincipOfLenguage.py
+++ ML/Python - Basics and Applications/BasePrincipOfLenguage.py
@@ -1,8 +1,30 @@
-ans = 0
-RepObjects = []
-for obj in objects:
-    if obj not in RepObjects:
-        RepObjects.append(obj)
-        ans += 1
+# ans = 0
+# RepObjects = []
+# for obj in objects:
+#     if obj not in RepObjects:
+#         RepObjects.append(obj)
+#         ans += 1
+#
+# print(ans)
 
-print(ans)
+# def function_name(argument1 , argument2):
+#     return argument1 + argument2
+# print(id(function_name(2,8)))
+# x = function_name(2,8)
+# print(id(x))
+# y = (function_name(8 , 2))
+# print(id(y))
+
+def list_sum (lst):
+    result = 0
+    for element in lst:
+        result += element
+    return result
+
+def sum(a,b):
+    return a + b
+
+y = sum(14,29)
+z = list_sum([1,2,3])
+print(y)
+print(z)
